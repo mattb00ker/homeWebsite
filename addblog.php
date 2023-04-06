774fdaed77436bdaf2e6aa50d5dd131cd72c7c89
Matt Booker <73802648+mattb00ker@users.noreply.github.com>
2023-04-06 17:00:27 +0100

    adding the blog and adding pages

---
+++ addblog.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add New Post</title>
+</head>
+<body>
+
+	<h1>Add New Post</h1>
+
+	<form method="post" action="add_post.php">
+		<label for="title">Title:</label>
+		<input type="text" name="title" id="title"><br><br>
+
+		<label for="author">Author:</label>
+		<input type="text" name="author" id="author"><br><br>
+
+		<label for="content">Content:</label>
+		<textarea name="content" id="content"></textarea><br><br>
+
+		<input type="submit" name="submit" value="Submit">
+	</form>
+
+</body>
+</html>
